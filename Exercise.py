a28f36b5faafad0b1c5fbde70fb190055154120b
Travis Byrman <travis-byrman@live.nmit.ac.nz>
2021-07-26 10:57:32 +1200

    saved

--- Exercise.py
+++ Exercise.py
@@ -11,17 +11,16 @@
 # fizz_buzz(2)
 
 def fizz_buzz(fizz=3, buzz=5, up_to=15):
-    x = 1
+    x = 0
     result = []
-    while x < up_to:
+    while x <= up_to:
         string = ""
         if x % 3 == 0:
             string += "Fizz"
         if x % 5 == 0:
             string += "Buzz"
-        result.append((x, (str(x) if string == "" else string)))
+        print((x, (str(x) if string == "" else string)))
         x += 1
-    return result
 
 
-print(fizz_buzz(3, 5, 100))
+fizz_buzz(3, 5, 56)
