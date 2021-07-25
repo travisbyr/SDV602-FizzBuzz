65a7de495f76c177db41622ed598ef94aa95fe98
Travis Byrman <travis-byrman@live.nmit.ac.nz>
2021-07-26 10:32:35 +1200

    Saved, completed activity

--- Exercise.py
+++ Exercise.py
@@ -1,21 +1,27 @@
-def fizz_buzz(number):
-    #fizz - multiples 3 /3
-    # buzz - multiples 5 /5
+# def fizz_buzz(number):
+#     if number % 3 == 0 and number % 5 == 0:
+#         print("fizzbuzz")
+#     elif number % 5 == 0:
+#         print("buzz")
+#     elif number % 3 == 0:
+#         print("fizz")
+#     else:
+#         print(number)
 
-    #buzz = number / 5
-    #fizz = number / 3
+# fizz_buzz(2)
 
-    if number % 3 == 0 and number % 5 == 0:
-        print("fizzbuzz")
-    elif number % 5 == 0:
-        print("buzz")
-    elif number % 3 == 0:
-        print("fizz")
-    else:
-        print(number)
+def fizz_buzz(fizz=3, buzz=5, up_to=15):
+    x = 1
+    result = []
+    while x < up_to:
+        string = ""
+        if x % 3 == 0:
+            string += "Fizz"
+        if x % 5 == 0:
+            string += "Buzz"
+        result.append((x, (str(x) if string == "" else string)))
+        x += 1
+    return result
 
 
-
-
-
-fizz_buzz(2)
+print(fizz_buzz(3, 5, 100))
